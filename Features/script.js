e33e47498e0f4538d77b175ca43c098e7c50f354
aldyakbar26 <akbaralluwiyadi@gmail.com>
2022-02-06 22:43:09 +0700

    create features section

---
+++ Features/script.js
@@ -0,0 +1,33 @@
+// NAVBAR
+const navbar = document.querySelector('.navbar');
+console.log(navbar);
+
+window.addEventListener('scroll', function () {
+    const navsPosition = window.scrollY;
+    console.log(navsPosition);
+
+    if (navsPosition > 10) {
+        navbar.classList.add('animation-navbar');
+    } else {
+        navbar.classList.remove('animation-navbar');
+    }
+})
+// END OF NAVBAR
+
+
+// MESSAGE
+const messageFromMe = document.querySelector('#message .container .message-from-me');
+console.log(messageFromMe);
+
+window.addEventListener('load', function () {
+    this.setTimeout(() => {
+        messageFromMe.classList.add('message-animation');
+    }, 3000)
+
+    messageFromMe.addEventListener('click', (e) => {
+        if (e.target.className == 'fas fa-times') {
+            messageFromMe.classList.remove('message-animation');
+        }
+    })
+})
+// END OF MESSAGE
